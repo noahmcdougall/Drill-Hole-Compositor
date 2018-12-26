465830fd301a30bf26af001a787d2f3d34582ab8
noahmcdougall <noahmcdougall@gmail.com>
2018-12-25 19:07:53 -0600

    Fuller runs
    Added parts to script to go back and check if adding the row prior to the run still grades higher than the cutoff.

--- Compositor.py
+++ Compositor.py
@@ -25,11 +25,13 @@ for i in holes:
     runlength = 0
     beginning = 0
     end = 0
+    iteratornum = 0
 
     ## Iterates through the rows only where the hole ID matches ##
     for j in range(0, len(datatable)):
         if datatable[j][0] == i:
             length = datatable[j][2]-datatable[j][1]
+            gobacklength = datatable[iteratornum][2]-datatable[iteratornum][1]
 
             ## Condition where we start into the first run ##
             if ((datatable[j][3]*length)+gradeton)/(runlength + length)>=cutoffgrade:
@@ -38,12 +40,17 @@ for i in holes:
                 gradeton = grade * runlength
                 if inorout == "out":
                     beginning = datatable[j][1]
+                    iteratornum = j-1
                 else:
                     end = datatable[j][2]
                 inorout = "in"
 
             ## Condition where we get to the end of the first run and commit the results ##
             if ((datatable[j][3]*length)+gradeton)/(runlength + length)<cutoffgrade and inorout == "in":
+                ## Once at the end of the run, it goes back to check if adding the row prior to the run (as long as it's greater than 0%) keeps the entire run above grade) ##
+                if iteratornum >= 0 and datatable[iteratornum][3] > 0 and ((datatable[iteratornum][3]*gobacklength)+gradeton)/(runlength + gobacklength)>=cutoffgrade:
+                    grade = ((datatable[iteratornum][3]*gobacklength)+gradeton)/(runlength + gobacklength)
+                    runlength = runlength + gobacklength
                 results.append((i, beginning, end, runlength, round(grade,2)))
                 runlength = 0
                 grade = 0
