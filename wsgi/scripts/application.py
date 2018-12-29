e2667568d6c7267567110bc73d55219ec98194ff
noahmcdougall <noahmcdougall@gmail.com>
2018-12-29 10:48:05 -0600

    index and application
    Attempting to request user input

--- wsgi/scripts/application.py
+++ wsgi/scripts/application.py
@@ -40,8 +40,8 @@ class calculate:
     ## Pulling in user inputs ##
     @cherrypy.expose
     def userinput(self, cutoffgradein, mincutoffgradein):
-        cutoffgrade = float(cutoffgradein)
-        mincutoffgrade = float(mincutoffgradein)
+        cutoffgrade = float(request.form['cutoffgradein'])
+        mincutoffgrade = float(request.form['mincutoffgradein'])
 
     ## Initial loading of data from csv ##
     @cherrypy.expose
